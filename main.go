cf8590c3a72ef17859250066457f8f278668591c
AnielHillebrand <aniel_igor@hotmail.com>
2023-09-11 14:49:55 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/AnielHill/goportunities/router"
+import (
+	"github.com/AnielHill/goportunities/config"
+	"github.com/AnielHill/goportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	//Initialize Router
 	router.Initialize()
